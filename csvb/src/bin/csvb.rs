e0ad527be235fd343f79469ffb5c4ed23a560dbd
A K <alex.kesling@gmail.com>
2025-01-27 21:50:28 -0500

    feat: haiku command and log levels flag

--- csvb/src/bin/csvb.rs
+++ csvb/src/bin/csvb.rs
@@ -1,3 +1,124 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::BTreeSet;
+
+use anyhow::{bail, Context as _, Result};
+use clap::Parser;
+
+#[derive(clap::Parser, Debug)]
+#[command(author, version, about, long_about = None, arg_required_else_help = true)]
+struct Args {
+    #[command(subcommand)]
+    command: Command,
+
+    /// Log level pairs of the form <MODULE>:<LEVEL>.
+    #[arg(long)]
+    log_levels: Option<Vec<String>>,
+}
+
+#[derive(clap::Subcommand, Debug)]
+enum Command {
+    /// Print a random haiku
+    Haiku(HaikuOptions),
+}
+
+#[derive(clap::Parser, Debug)]
+struct HaikuOptions {
+    /// Print all haikus
+    #[arg(long)]
+    all: bool,
+}
+
+/// Print a random CSV-related haiku
+fn print_haiku(options: &HaikuOptions) {
+    use rand::seq::SliceRandom as _;
+
+    println!("line 1: line 2: line 3");
+    if options.all {
+        for h in csvb::HAIKUS {
+            println!("{}", h.join(":"))
+        }
+    } else {
+        let mut rng = rand::thread_rng();
+        println!(
+            "{}",
+            csvb::HAIKUS
+                .choose(&mut rng)
+                .expect("at least one haiku")
+                .join(":")
+        )
+    }
+}
+
+/// Convert a series of <MODULE>:<LEVEL> pairs into actionable `(module, LevelFilter)` pairs
+fn as_level_pairs(config: &[String]) -> Result<Vec<(&str, simplelog::LevelFilter)>> {
+    let mut pairs = Vec::with_capacity(config.len());
+    for c in config {
+        let tokens: Vec<&str> = c.split(":").collect();
+        if tokens.len() != 2 {
+            bail!("Flag config pair was not of the form <MODULE>:<LEVEL>: '{c}'");
+        }
+        pairs.push((
+            tokens[0],
+            match tokens[1].to_lowercase().as_str() {
+                "trace" => simplelog::LevelFilter::Trace,
+                "debug" => simplelog::LevelFilter::Debug,
+                "info" => simplelog::LevelFilter::Info,
+                "warn" => simplelog::LevelFilter::Warn,
+                "error" => simplelog::LevelFilter::Error,
+                _ => bail!("Unrecognized level name in '{c}'"),
+            },
+        ))
+    }
+
+    Ok(pairs)
+}
+
+fn initialize_logging(
+    module_path_filters: &[(&str, simplelog::LevelFilter)],
+) -> anyhow::Result<()> {
+    simplelog::CombinedLogger::init(
+        module_path_filters
+            .into_iter()
+            .map(|(module_path_filter, level)| {
+                simplelog::TermLogger::new(
+                    *level,
+                    simplelog::ConfigBuilder::new()
+                        .add_filter_allow(module_path_filter.to_string())
+                        .build(),
+                    simplelog::TerminalMode::Mixed,
+                    simplelog::ColorChoice::Auto,
+                ) as Box<dyn simplelog::SharedLogger>
+            })
+            .collect(),
+    )
+    .map_err(|e| e.into())
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+    let levels_arg: Option<Vec<String>> = args.log_levels;
+
+    let log_levels: Vec<(&str, simplelog::LevelFilter)> = {
+        let mut log_levels = BTreeSet::from([
+            ("csvb", simplelog::LevelFilter::Debug),
+            // If compiled via Bazel, "csvb" binary crate module will be named "bin"
+            ("bin", simplelog::LevelFilter::Debug),
+        ]);
+
+        for level in levels_arg
+            .as_deref()
+            .map(as_level_pairs)
+            .unwrap_or(Ok(vec![]))?
+        {
+            log_levels.insert(level);
+        }
+        log_levels.into_iter().collect()
+    };
+    let _ = initialize_logging(&log_levels[..]);
+    log::trace!("Logging initialized, commands parsed...");
+
+    match args.command {
+        Command::Haiku(options) => print_haiku(&options),
+    }
+    Ok(())
 }
--- csvb/src/lib.rs
+++ csvb/src/lib.rs
@@ -1,1 +1,52 @@
-
+pub static HAIKUS: [[&str; 3]; 10] = [
+    [
+        "Columns fall like rain,",
+        "Rows unroll in endless streams —",
+        "Order from chaos.",
+    ],
+    [
+        "Comma on the loose,",
+        "Sharp precision, splitting words —",
+        "Language chopped to bits.",
+    ],
+    [
+        "Rows become columns,",
+        "Twisting, spinning, transforming —",
+        "New perspectives bloom.",
+    ],
+    [
+        "Malformed CSV,",
+        "Spaces, quotes, and hidden tabs —",
+        "Parser's nightmare realm.",
+    ],
+    [
+        "Count, sum, group by joy,",
+        "Data folds to tell its tale —",
+        "Metrics rule the world.",
+    ],
+    [
+        "Columns never named,",
+        "Yet their whispers fill the air —",
+        "Untold truths await.",
+    ],
+    [
+        "Files collide with glee,",
+        "VLOOKUP and JOIN entwine —",
+        "One table reborn.",
+    ],
+    [
+        "CSV’s raw heart,",
+        "Plotted as a vivid bloom —",
+        "Insight takes its root.",
+    ],
+    [
+        "Scroll forevermore,",
+        "Endless sheets of raw data —",
+        "Time lost in numbers.",
+    ],
+    [
+        "Data distilled pure;",
+        "Saved, .csv treasure —",
+        "A programmer’s peace.",
+    ],
+];
