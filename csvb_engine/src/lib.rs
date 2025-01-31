903fccfcadc1fc22133db06c27019b28bad26a7e
Alex Kesling <alex@kesling.co>
2025-01-31 15:24:23 -0500

    refactor: factor out an "Engine" trait to hold state across queries

--- csvb_engine/src/lib.rs
+++ csvb_engine/src/lib.rs
@@ -1,50 +1,87 @@
 use anyhow::{anyhow, bail};
 use std::sync::Arc;
 
+use datafusion::{execution::SendableRecordBatchStream, prelude::SessionContext, sql::sqlparser};
+
+#[async_trait::async_trait]
+pub trait Engine {
+    async fn execute(&mut self, query: &str) -> anyhow::Result<SendableRecordBatchStream>;
+}
+
+pub struct CsvbEngine {
+    context: SessionContext,
+}
+
+impl CsvbEngine {
+    #[allow(clippy::new_ret_no_self)]
+    pub async fn new(
+        sources: Vec<String>,
+        memory_limit_bytes: usize,
+    ) -> anyhow::Result<Box<dyn Engine>> {
+        use datafusion::prelude::*;
+        let session_config = SessionConfig::from_env()?.with_information_schema(true);
+        let mut rt_builder = datafusion::execution::runtime_env::RuntimeEnvBuilder::new();
+        rt_builder = rt_builder.with_memory_pool(Arc::new(
+            datafusion::execution::memory_pool::GreedyMemoryPool::new(memory_limit_bytes),
+        ));
+
+        let runtime_env = rt_builder.build_arc()?;
+        let context = SessionContext::new_with_config_rt(session_config, runtime_env);
+        let csv_format = datafusion::datasource::file_format::csv::CsvFormat::default();
+        let listing_options =
+            datafusion::datasource::listing::ListingOptions::new(Arc::new(csv_format))
+                .with_file_extension(".csv");
+
+        let table_paths: Vec<_> = sources
+            .iter()
+            .map(datafusion::datasource::listing::ListingTableUrl::parse)
+            .collect::<Result<_, _>>()
+            .map_err(|err| anyhow!("{err}"))?;
+        let resolved_schema = listing_options
+            .infer_schema(&context.state(), &table_paths[0])
+            .await?;
+        let config =
+            datafusion::datasource::listing::ListingTableConfig::new_with_multi_paths(table_paths)
+                .with_listing_options(listing_options)
+                .with_schema(resolved_schema);
+        let listing_table = datafusion::datasource::listing::ListingTable::try_new(config)?;
+
+        context.register_table("tbl", Arc::new(listing_table))?;
+
+        Ok(Box::new(CsvbEngine { context }))
+    }
+}
+
+#[async_trait::async_trait]
+impl Engine for CsvbEngine {
+    async fn execute(&mut self, query: &str) -> anyhow::Result<SendableRecordBatchStream> {
+        Ok(self.context.sql(query).await?.execute_stream().await?)
+    }
+}
+
 pub struct CmdOptions {
     /// The number of bytes the command memory pool should be limited to
     pub memory_limit_bytes: usize,
 }
 
 pub async fn run_cmd(options: &CmdOptions, sources: Vec<String>, sql: &str) -> anyhow::Result<()> {
-    use datafusion::prelude::*;
+    use futures::stream::StreamExt as _;
 
     if sources.is_empty() {
         bail!("No sources provided when running command")
     }
 
-    let session_config = SessionConfig::from_env()?.with_information_schema(true);
-    let mut rt_builder = datafusion::execution::runtime_env::RuntimeEnvBuilder::new();
-    rt_builder = rt_builder.with_memory_pool(Arc::new(
-        datafusion::execution::memory_pool::GreedyMemoryPool::new(options.memory_limit_bytes),
-    ));
+    let mut engine = CsvbEngine::new(sources, options.memory_limit_bytes).await?;
+    let mut stream = engine.execute(sql).await?;
+    let mut batches = Vec::new();
+    while let Some(items) = stream.next().await {
+        batches.push(items?);
+    }
 
-    let runtime_env = rt_builder.build_arc()?;
-    let ctx = SessionContext::new_with_config_rt(session_config, runtime_env);
-    let csv_format = datafusion::datasource::file_format::csv::CsvFormat::default();
-    let listing_options =
-        datafusion::datasource::listing::ListingOptions::new(Arc::new(csv_format))
-            .with_file_extension(".csv");
-
-    let table_paths: Vec<_> = sources
-        .iter()
-        .map(datafusion::datasource::listing::ListingTableUrl::parse)
-        .collect::<Result<_, _>>()
-        .map_err(|err| anyhow!("{err}"))?;
-    let resolved_schema = listing_options
-        .infer_schema(&ctx.state(), &table_paths[0])
-        .await?;
-    let config =
-        datafusion::datasource::listing::ListingTableConfig::new_with_multi_paths(table_paths)
-            .with_listing_options(listing_options)
-            .with_schema(resolved_schema);
-    let listing_table = datafusion::datasource::listing::ListingTable::try_new(config)?;
-
-    ctx.register_table("tbl", Arc::new(listing_table))?;
-    // TODO(alex): Add UDF to print haiku
-    //
-    let df = ctx.sql(sql).await?;
-    let batches = df.collect().await?;
+    //while let Some(batch) = stream.next().await {
+    //    let pretty_results = arrow::util::pretty::pretty_format_batches(&[items?])?.to_string();
+    //    println!("Results:\n{}", pretty_results);
+    //}
 
     let pretty_results = arrow::util::pretty::pretty_format_batches(&batches[..])?.to_string();
     println!("Results:\n{}", pretty_results);
